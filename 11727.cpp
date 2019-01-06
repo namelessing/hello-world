df80b5ece2c04368ecab30bd7d3512699a19d701
namelessing <46418999+namelessing@users.noreply.github.com>
2019-01-06 22:41:22 +0900

    Create 11727.cpp

---
+++ 11727.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+int dp[1003] = {0,};
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+
+	dp[1] = 1;
+	dp[2] = 3;
+
+	for(int i = 3; i <= n; i++) {
+		dp[i] = (dp[i-1] + dp[i-2]*2) % 10007;
+	}
+
+	printf("%d\n", dp[n] % 10007);
+
+	return 0;
+}
